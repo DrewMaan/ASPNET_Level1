d3a46455d3bb43ed10b1ff3f7b4d6e504c863a73
Andrey <kasimovskiy.a.d.public@gmail.com>
2021-12-20 00:23:06 +0300

    Setting up middleware

--- WebStore/WebStore/Program.cs
+++ WebStore/WebStore/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();    // Промежуточное ПО. Визуализация exceptions в браузере.
+}
+
+app.UseRouting();
+
+//app.MapControllerRoute();
 
 app.Run();
