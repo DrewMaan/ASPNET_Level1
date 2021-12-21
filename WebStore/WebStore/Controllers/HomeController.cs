d32b86c963b17b1829803bc5095c1e499c11769c
Andrey <kasimovskiy.a.d.public@gmail.com>
2021-12-21 22:41:13 +0300

    Реализация страниц информации о сотруднике.

--- WebStore/WebStore/Controllers/HomeController.cs
+++ WebStore/WebStore/Controllers/HomeController.cs
@@ -7,9 +7,12 @@ namespace WebStore.Controllers
 	{
 		private static readonly List<Employee> _employees = new List<Employee>
 		{
-			new Employee { Id = 1, LastName = "Ivanov", FirstName = "Ivan", Patronymic = "Ivanovich", Age = 30 },
-			new Employee { Id = 2, LastName = "Petrov", FirstName = "Petr", Patronymic = "Petrovich", Age = 45 },
-			new Employee { Id = 3, LastName = "Sidorov", FirstName = "Sidor", Patronymic = "Sidorovich", Age = 24 }
+			new Employee { Id = 1, LastName = "Ivanov", FirstName = "Ivan", Patronymic = "Ivanovich", Age = 30, 
+							Department = "HR department", Position = "HR BP", StartDate = new DateTime(2020, 10, 23)},
+			new Employee { Id = 2, LastName = "Petrov", FirstName = "Petr", Patronymic = "Petrovich", Age = 45,
+							Department = "Accounting department", Position = "Accountant", StartDate = new DateTime(2019, 12, 10)},
+			new Employee { Id = 3, LastName = "Sidorov", FirstName = "Sidor", Patronymic = "Sidorovich", Age = 24,
+							Department = "Developing department", Position = "Developer", StartDate = new DateTime(2021, 05, 23)}
 		};
 
 		public IActionResult Index()
@@ -25,6 +28,11 @@ namespace WebStore.Controllers
 		public IActionResult Employees()
 		{
 			return View(_employees);
+		}
+
+		public IActionResult Employee(int id)
+		{
+			return View(_employees.FirstOrDefault(x => x.Id == id));
 		}
 	}
 }
--- WebStore/WebStore/Models/Employee.cs
+++ WebStore/WebStore/Models/Employee.cs
@@ -7,5 +7,10 @@
 		public string FirstName { get; set; }
 		public string Patronymic { get; set; }
 		public int Age { get; set; }
+
+		public string Department { get; set; }
+		public string Position { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
 	}
 }
