c2089ee95eddabff12830d1208fa9fe7c910ed47
Andrey <kasimovskiy.a.d.public@gmail.com>
2021-12-21 21:30:10 +0300

    Добавлен список сотрудников

--- WebStore/WebStore/Controllers/HomeController.cs
+++ WebStore/WebStore/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
 	public class HomeController : Controller
 	{
+		private static readonly List<Employee> _employees = new List<Employee>
+		{
+			new Employee { Id = 1, LastName = "Ivanov", FirstName = "Ivan", Patronymic = "Ivanovich", Age = 30 },
+			new Employee { Id = 2, LastName = "Petrov", FirstName = "Petr", Patronymic = "Petrovich", Age = 45 },
+			new Employee { Id = 3, LastName = "Sidorov", FirstName = "Sidor", Patronymic = "Sidorovich", Age = 24 }
+		};
+
 		public IActionResult Index()
 		{
 			return View();
@@ -12,6 +20,11 @@ namespace WebStore.Controllers
 		public IActionResult CofiguredAction(string id, string value1)
 		{
 			return Content($"Hello world! {id} - {value1}");
+		}
+
+		public IActionResult Employees()
+		{
+			return View(_employees);
 		}
 	}
 }
